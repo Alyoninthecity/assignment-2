b14bedaa04c4fd819330a44e3d9a811cd6a4bcc1
stefano.bressan@hotmail.it <stefano.bressan@hotmail.it>
2023-05-18 03:41:33 +0200

    close #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -10,8 +10,30 @@ package it.unipd.mtss;
 /** Gestire un input non valido (<1 e >1000) */
 public class IntegerToRoman {
 
-    public static String convert(int number) {
-        // TODO
-        return null;
+    private static final int[] ArrayNumeri = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    private static final String[] ArraySimboli = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV",
+            "I" };
+
+    public static String convert(int numeroArabo) {
+        try {
+            if (numeroArabo < 1) {
+                throw new Exception("Numero troppo piccolo < 1!!");
+            }
+            if (numeroArabo > 1000) {
+                throw new Exception("Numero troppo grande > 1000!!");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return "Errore";
+        }
+        StringBuilder risultato = new StringBuilder();
+
+        for (int i = 0; i < ArrayNumeri.length; i++) {
+            while (numeroArabo >= ArrayNumeri[i]) {
+                risultato.append(ArraySimboli[i]);
+                numeroArabo -= ArrayNumeri[i];
+            }
+        }
+        return risultato.toString();
     }
 }
